782a26cf764ab6300ac7bd2189c0e8f6114af52b
Millurr <42452986+Millurr@users.noreply.github.com>
2021-05-13 23:32:23 -0500

    Converted Home Page

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import { FaLinkedin } from 'react-icons/fa';
 
 export default function Footer() {
     return (
         <footer>
-            <p><b>Email: </b>josh.miller@selu.edu | <b>Phone: </b>228-216-6215</p>
+            <p><b>Email: </b>josh.miller@selu.edu | <b>Phone: </b>228-216-6215 </p> 
+            <a href="https://www.linkedin.com/in/josh-miller-2351b0201/" target="_blank"><FaLinkedin/></a>
         </footer>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,17 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import content from './content.json'
 
-export default function Home() {
-    let data = content.data;
+interface Projects {
+    language: string;
+    details: {
+        project: string,
+        description: string,
+        link: string
+    }[];
+}
+
+export default function Home(): JSX.Element {
+    let data: Projects[] = content.content;
 
     return (
         <div>
@@ -19,12 +28,12 @@ export default function Home() {
             <div>
 
                 {
-                    data.map((languages, idx) => (
+                    data.map((languages, idx: number) => (
                         <div key={idx}>
                             <h2 className={styles.subtitle}>{languages.language}</h2>
                             <div className={styles.cards}>
                                 {
-                                    languages.details.map((details, idx) => (
+                                    languages.details.map((details, idx: number) => (
                                         Card(details, idx)
                                     ))
                                 }
@@ -41,9 +50,9 @@ export default function Home() {
     )
 }
 
-function Card({ link, project, description }, idx) {
+const Card = ({ link, project, description }, idx: number) => {
     return (  
-        <div className={styles.card} key={project}>
+        <div className={styles.card} key={idx}>
             <a href={link} target="_blank">
                 {/* <Image src="img_avatar.png" alt="Avatar" style="width:100%"/> */}
                 <div>
