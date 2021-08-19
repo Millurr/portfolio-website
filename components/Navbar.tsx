5b8c36c43b848f35cfa04e42ff47ead7b924c71e
Josh <millurr0@gmail.com>
2021-08-19 17:12:34 -0500

    Changed theme and updated some content

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
+import { FaLinkedin, FaGithub, FaEnvelope, FaPhone } from 'react-icons/fa';
 
 const Navbar = () => {
 
     return (
         <nav>
-            <div className="brand">Josh Miller</div>
+            <div className="brand">Josh Miller <a href="mailto: josh.miller@selu.edu" target="_blank"><FaEnvelope /></a><a href="tel:228-216-6215"><FaPhone /></a>
+                <a href="https://www.linkedin.com/in/josh-miller-2351b0201/" target="_blank"><FaLinkedin/></a>
+                <a href="https://github.com/Millurr" target="_blank"><FaGithub/></a>
+            </div>
             <div className="links">
                 <Link href="/"><a>Projects</a></Link>
                 <Link href="/about"><a>About</a></Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import sweetStyles from '../styles/SweetAlert.module.css';
 import content from './content.json';
-import swal from '@sweetalert/with-react';
-import { SweetAlert } from '../components/SweetAlert';
 
 interface Projects {
     language: string;
@@ -56,30 +52,19 @@ const Home = (): JSX.Element => {
 }
 
 const Card = ({ link, project, description, images }, idx: number): JSX.Element => {
-    const showSwal = (title: string, desc: string, imgs: {img: string}[], lnk: string) => {
-        swal({
-            className: sweetStyles.container,
-            buttons: {
-                cancel: {
-                    text: "Close",
-                    value: true,
-                    visible: true,
-                    className: sweetStyles.button,
-                    closeModal: true,
-                  }
-              },
-            content:<SweetAlert title={title} description={desc} link={lnk} imgs={imgs}/>
-        })
-    }
     
     return (  
-        <div className={styles.card} key={idx} onClick={() => showSwal(project, description, images, link)}>
+        <div className={styles.card} key={idx}>
                 {/* <Image src="img_avatar.png" alt="Avatar" style="width:100%"/> */}
                 <div>
                     <h4>
                         <b>{project}</b>
                     </h4>
-                    {/* <p>{description}</p> */}
+                    <p>
+                        {description}
+                    </p>
+
+                    <a className={styles.cardlink} href={link} target="_blank">Source code</a>
                 </div>
         </div>
     )
